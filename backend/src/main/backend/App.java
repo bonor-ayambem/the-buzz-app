fdf81474a3635015606acb228461731464bae44c
bonor-ayambem <78880733+bonor-ayambem@users.noreply.github.com>
2023-05-18 00:11:51 -0400

    Update App.java

--- backend/src/main/backend/App.java
+++ backend/src/main/backend/App.java
@@ -123,19 +123,37 @@ public class App {
         */
         
         Database dataStore = Database.getDatabase(db_url);        
-        //PHASE 2
         
-        dataStore.dropTableUser();
-        dataStore.createTableUser();
+        Table userTable = new Table("tblUser");
+        Table messageTable = new Table("tblMessages");
+        Table commentTable = new Table("tblComment");
+        Table likesTable = new Table("tblLikes");
+
+
+        dataStore.dropTable(userTable);
+        dataStore.createTable(userTable); 
+
+        dataStore.dropTable(messageTable);
+        dataStore.createTable(messageTable); 
         
-        dataStore.dropTableMessages();
-        dataStore.createTableMessages();
+        dataStore.dropTable(commentTable);
+        dataStore.createTable(commentTable); 
 
-        dataStore.dropTableComment();
-        dataStore.createTableComment();
+        dataStore.dropTable(likesTable);
+        dataStore.createTable(likesTable); 
 
-        dataStore.dropTableLikes();
-        dataStore.createTableLikes();
+        
+        // dataStore.dropTableUser();
+        // dataStore.createTableUser();
+        
+        // dataStore.dropTableMessages();
+        // dataStore.createTableMessages();
+
+        // dataStore.dropTableComment();
+        // dataStore.createTableComment();
+
+        // dataStore.dropTableLikes();
+        // dataStore.createTableLikes();
 
         // Set up the location for serving static files.  If the STATIC_LOCATION
         // environment variable is set, we will serve from it.  Otherwise, serve
