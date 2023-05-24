0469c8246cd5e95d4d1d2de6c04d43a685af0540
Bonor Ayambem <bma223@lehigh.edu>
2023-05-24 01:41:33 -0400

    create() method?

--- backend/src/main/backend/Table.java
+++ backend/src/main/backend/Table.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Table {
     private String name;
+    private Database db;
 
     private String deleteStatement;
     private String updateStatement;
@@ -17,8 +18,9 @@ public class Table {
     private PreparedStatement mUpdateRow;
     // Other prepared statements...
 
-    public Table(String tName){
+    public Table(String tName, Database db){
         name = tName;
+        this.db = db;
         deleteStatement = "DELETE FROM " + name + " WHERE row_id = ?";
         updateStatement = "UPDATE " + name + " SET content = ? WHERE row_id = ?";
     }
@@ -27,29 +29,31 @@ public class Table {
         return name;
     }
 
-    public void create(Database db) {
-        try {
-            if(name.equals("tblMessages")) {
-                db.mCreateTable = db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblMessages (row_id SERIAL PRIMARY KEY, subject VARCHAR(50) NOT NULL, content VARCHAR(500) NOT NULL, user_id INTEGER REFERENCES tblUsers)");
-            }
-            else if(name.equals("tblComment")) {
-                db.mCreateTable =  db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblComment (row_id SERIAL PRIMARY KEY, message_id INTEGER REFERENCES tblMessages, user_id INTEGER REFERENCES tblUsers, content VARCHAR(500) NOT NULL)");
-            }
-            else if(name.equals("tblUsers")) {
-                db.mCreateTable = db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblUsers (row_id SERIAL PRIMARY KEY, user_name VARCHAR(500) NOT NULL, first_name VARCHAR(500) NOT NULL, last_name VARCHAR(500) NOT NULL, email VARCHAR(500) NOT NULL)");
-            }
-            else if(name.equals("tblLikes")) {
-                db.mCreateTable = db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblLikes (row_id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES tblUsers, message_id INTEGER REFERENCES tblMessages, content INTEGER)");    
-            }
+// will delete after confirmation that approach of creating the table in the database
+// before the Table object is good
+    // public void create(Database db) {
+    //     try {
+    //         if(name.equals("tblMessages")) {
+    //             db.mCreateTable = db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblMessages (row_id SERIAL PRIMARY KEY, subject VARCHAR(50) NOT NULL, content VARCHAR(500) NOT NULL, user_id INTEGER REFERENCES tblUsers)");
+    //         }
+    //         else if(name.equals("tblComment")) {
+    //             db.mCreateTable =  db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblComment (row_id SERIAL PRIMARY KEY, message_id INTEGER REFERENCES tblMessages, user_id INTEGER REFERENCES tblUsers, content VARCHAR(500) NOT NULL)");
+    //         }
+    //         else if(name.equals("tblUsers")) {
+    //             db.mCreateTable = db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblUsers (row_id SERIAL PRIMARY KEY, user_name VARCHAR(500) NOT NULL, first_name VARCHAR(500) NOT NULL, last_name VARCHAR(500) NOT NULL, email VARCHAR(500) NOT NULL)");
+    //         }
+    //         else if(name.equals("tblLikes")) {
+    //             db.mCreateTable = db.mConnection.prepareStatement("CREATE TABLE IF NOT EXISTS tblLikes (row_id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES tblUsers, message_id INTEGER REFERENCES tblMessages, content INTEGER)");    
+    //         }
 
-            db.mCreateTable.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+    //         db.mCreateTable.execute();
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
-    public void drop(Database db) {
-        String statement = "DROP TABLE IF EXISTS " + name + " CASCADE;"; // Added space before CASCADE
+    public void drop() {
+        String statement = "DROP TABLE IF EXISTS " + name + " CASCADE;";
         try {
             db.mDropTable = db.mConnection.prepareStatement(statement);
             db.mDropTable.execute();
@@ -58,7 +62,7 @@ public class Table {
         }
     }
 
-    public int insertRow(Database db, ArrayList<String> content) {
+    public int insertRow(ArrayList<String> content) {
         StringJoiner statement = new StringJoiner(", ", "INSERT INTO " + name + " VALUES (default,", ")");
 
         for (int i = 0; i < content.size(); i++) {
@@ -77,7 +81,7 @@ public class Table {
         return 0;
     }
 
-    public int updateRow(Database db, int row_id, String content) {
+    public int updateRow(int row_id, String content) {
         try {
             db.mUpdateRow = db.mConnection.prepareStatement(updateStatement);
             db.mUpdateRow.setString(1, content);
@@ -89,7 +93,7 @@ public class Table {
         return 0;
     }
 
-    public int deleteRow(Database db, int row_id) {
+    public int deleteRow(int row_id) {
         try {
             db.mDeleteRow = db.mConnection.prepareStatement(deleteStatement);
             db.mDeleteRow.setInt(1, row_id);
